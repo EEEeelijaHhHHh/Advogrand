1dc8931f48b80e2cbc7a5bbab689bcd29280b32d
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-09-21 13:46:43 +0300

    Reviews Section with slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(() => {
 
+  /* Parners Slider */
   $('.partners').slick({
     infinite: true,
     slidesToShow: 4,
     prevArrow: $('.partners__arrow_left'),
     nextArrow: $('.partners__arrow_right')
   });
+
+  /* Reviews Slider */
+  $('.reviews-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    prevArrow: $('.reviews-slider__arrow_left'),
+    nextArrow: $('.reviews-slider__arrow_right')
+  });
+
+  /* Reviews Tabs Filtering */
 
 });
